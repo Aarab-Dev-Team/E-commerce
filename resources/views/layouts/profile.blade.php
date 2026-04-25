85e00d8b8be52096231a8188038ea222d18f9ddd
MouadDev12 <mouadmekrech72@gmail.com>
2026-04-25 09:42:38 +0100

    fix: remove invalid XML declaration from SVG icons in navbar

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -356,9 +356,30 @@
 
                 {{-- Desktop nav --}}
                 <nav class="header-nav">
-                    <a href="{{ route('shop.catalog') }}" class="{{ request()->routeIs('shop.*') ? 'nav-active' : '' }}"><?xml version="1.0" encoding="UTF-8"?><svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M3 10V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V10" stroke="#000000" stroke-width="1.5"></path><path d="M14.8333 21V15C14.8333 13.8954 13.9379 13 12.8333 13H10.8333C9.72874 13 8.83331 13.8954 8.83331 15V21" stroke="#000000" stroke-width="1.5" stroke-miterlimit="16"></path><path d="M21.8183 9.36418L20.1243 3.43517C20.0507 3.17759 19.8153 3 19.5474 3H15.5L15.9753 8.70377C15.9909 8.89043 16.0923 9.05904 16.2532 9.15495C16.6425 9.38698 17.4052 9.81699 18 10C19.0158 10.3125 20.5008 10.1998 21.3465 10.0958C21.6982 10.0526 21.9157 9.7049 21.8183 9.36418Z" stroke="#000000" stroke-width="1.5"></path><path d="M14 10C14.5675 9.82538 15.2879 9.42589 15.6909 9.18807C15.8828 9.07486 15.9884 8.86103 15.9699 8.63904L15.5 3H8.5L8.03008 8.63904C8.01158 8.86103 8.11723 9.07486 8.30906 9.18807C8.71207 9.42589 9.4325 9.82538 10 10C11.493 10.4594 12.507 10.4594 14 10Z" stroke="#000000" stroke-width="1.5"></path><path d="M3.87567 3.43517L2.18166 9.36418C2.08431 9.7049 2.3018 10.0526 2.6535 10.0958C3.49916 10.1998 4.98424 10.3125 6 10C6.59477 9.81699 7.35751 9.38698 7.74678 9.15495C7.90767 9.05904 8.00913 8.89043 8.02469 8.70377L8.5 3H4.45258C4.18469 3 3.94926 3.17759 3.87567 3.43517Z" stroke="#000000" stroke-width="1.5"></path></svg> Shop</a>
-                    <a href="{{ route('cart.index') }}" class="{{ request()->routeIs('cart.*') ? 'nav-active' : '' }}"><?xml version="1.0" encoding="UTF-8"?><svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M19.2609 9.69589L20.6455 18.6959C20.8319 19.9074 19.8945 21 18.6688 21H5.33122C4.10545 21 3.16809 19.9074 3.35448 18.6959L4.73909 9.69589C4.8892 8.72022 5.7287 8 6.71584 8H17.2842C18.2713 8 19.1108 8.72022 19.2609 9.69589Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M14 5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg> Cart</a>
-                </nav>
+<a href="{{ route('shop.catalog') }}" 
+   class="{{ request()->routeIs('shop.*') ? 'nav-active' : '' }}">
+    
+    <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+        <path d="M3 10V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V10" stroke="#000000" stroke-width="1.5"></path>
+        <path d="M14.8333 21V15C14.8333 13.8954 13.9379 13 12.8333 13H10.8333C9.72874 13 8.83331 13.8954 8.83331 15V21" stroke="#000000" stroke-width="1.5" stroke-miterlimit="16"></path>
+        <path d="M21.8183 9.36418L20.1243 3.43517C20.0507 3.17759 19.8153 3 19.5474 3H15.5L15.9753 8.70377C15.9909 8.89043 16.0923 9.05904 16.2532 9.15495C16.6425 9.38698 17.4052 9.81699 18 10C19.0158 10.3125 20.5008 10.1998 21.3465 10.0958C21.6982 10.0526 21.9157 9.7049 21.8183 9.36418Z" stroke="#000000" stroke-width="1.5"></path>
+        <path d="M14 10C14.5675 9.82538 15.2879 9.42589 15.6909 9.18807C15.8828 9.07486 15.9884 8.86103 15.9699 8.63904L15.5 3H8.5L8.03008 8.63904C8.01158 8.86103 8.11723 9.07486 8.30906 9.18807C8.71207 9.42589 9.4325 9.82538 10 10C11.493 10.4594 12.507 10.4594 14 10Z" stroke="#000000" stroke-width="1.5"></path>
+        <path d="M3.87567 3.43517L2.18166 9.36418C2.08431 9.7049 2.3018 10.0526 2.6535 10.0958C3.49916 10.1998 4.98424 10.3125 6 10C6.59477 9.81699 7.35751 9.38698 7.74678 9.15495C7.90767 9.05904 8.00913 8.89043 8.02469 8.70377L8.5 3H4.45258C4.18469 3 3.94926 3.17759 3.87567 3.43517Z" stroke="#000000" stroke-width="1.5"></path>
+    </svg>
+
+    Shop
+</a>
+
+<a href="{{ route('cart.index') }}" 
+   class="{{ request()->routeIs('cart.*') ? 'nav-active' : '' }}">
+    
+    <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+        <path d="M19.2609 9.69589L20.6455 18.6959C20.8319 19.9074 19.8945 21 18.6688 21H5.33122C4.10545 21 3.16809 19.9074 3.35448 18.6959L4.73909 9.69589C4.8892 8.72022 5.7287 8 6.71584 8H17.2842C18.2713 8 19.1108 8.72022 19.2609 9.69589Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+        <path d="M14 5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+    </svg>
+
+    Cart
+</a>                </nav>
 
                 {{-- Hamburger (mobile only) --}}
                 <button class="nav-toggle" id="navToggle" aria-label="Open menu" aria-expanded="false">
@@ -418,15 +439,15 @@
                         @endif
 
                     @endauth
-                    <!-- <li><a href="{{ route('profile.edit') }}" class="{{ request()->routeIs('profile.edit') ? 'active' : '' }}"><i class="iconoir-user"></i> Profile info</a></li> -->
+                    <li><a href="{{ route('profile.edit') }}" class="{{ request()->routeIs('profile.edit') ? 'active' : '' }}"><i class="iconoir-user"></i> Profile info</a></li>
                     
 
                     @auth 
                         @if(auth()->user()->role==="customer")
 
-                                <li><a href="{{ route('profile.orders') }}"><i class="iconoir-box-iso"></i> Orders</a></li>
-                                <li><a href="{{ route('profile.addresses.index') }}"><i class="iconoir-map-pin"></i> Addresses</a></li>
-                                <li><a href="{{ route('profile.wishlist.index') }}"><i class="iconoir-heart"></i> Wishlist</a></li>
+                                <li><a href="{{ route('profile.orders') }}" class="{{ request()->routeIs('profile.orders') ? 'active' : '' }}"><i class="iconoir-box-iso"></i> Orders</a></li>
+                                <li><a href="{{ route('profile.addresses.index') }}" class="{{ request()->routeIs('profile.addresses.index') ? 'active' : '' }}"><i class="iconoir-map-pin"></i> Addresses</a></li>
+                                <li><a href="{{ route('profile.wishlist.index') }}" class="{{ request()->routeIs('profile.wishlist.index') ? 'active' : '' }}"><i class="iconoir-heart"></i> Wishlist</a></li>
                         @endif
 
                     @endauth
